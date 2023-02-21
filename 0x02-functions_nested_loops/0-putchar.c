ec492561f1a61dbe978935bbe9121870689721c7
AyaMohammedAli <sci.Ayamohammed01643@alexu.edu.eg>
2023-02-21 13:07:11 -0800

    oo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "_putchar.h"
 
 /**
  * main - Entry point
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	_putchar("_putchar");
+        _putchar('_putchar');
 	_putchar('\n');
 	return (0);
 }
