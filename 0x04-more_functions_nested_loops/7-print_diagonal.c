bb7ba947a4f1a20f1ed1025a78c8a2f6955a6162
AyaMohammedAli <sci.Ayamohammed01643@alexu.edu.eg>
2023-02-23 08:36:54 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,13 +4,24 @@
  *
  * @n: integer to print stuff?
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int i;
+	int j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-	     _putchar("\\");
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
