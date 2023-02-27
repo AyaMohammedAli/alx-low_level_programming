af236bc0f5431b9d6d7b76662e864b832b676638
AyaMohammedAli <sci.Ayamohammed01643@alexu.edu.eg>
2023-02-26 22:20:11 -0800

    update pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - update pointer value
+ * Return: return 0
+ */
+#include <stdio.h>
+
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
+
+
+  
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * reset_to_98 - update pointer value 
+ * Return :0
+ */
+void reset_to_98(int *n);
+#endif
