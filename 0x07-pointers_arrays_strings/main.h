a8d0c5c5494a06ef0ea27125f6700d708c9cb206
AyaMohammedAli <sci.Ayamohammed01643@alexu.edu.eg>
2023-03-07 12:33:51 -0800

    mem0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/**
+ * _memset - sets memory to array
+ * @s: array to set
+ * @b: value to set it as
+ * @n: n amount of times
+ * Return: char value of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+#endif
