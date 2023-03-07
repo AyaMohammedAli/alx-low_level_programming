ca7a6c25dd6c06409b0bfaaccd92c5e523c1d1f3
AyaMohammedAli <sci.Ayamohammed01643@alexu.edu.eg>
2023-03-07 12:39:32 -0800

    1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,12 @@
  * Return: char value of s
  */
 char *_memset(char *s, char b, unsigned int n);
+/**
+ * _memcpy - copies from source to dest values in array
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * @n: n amount of times
+ * Return: return the string value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
